80f7604a7f751aafd27d7929bce6c2eabce283f1
JimOhman <49884398+JimOhman@users.noreply.github.com>
2021-01-22 19:39:15 +0100

    refactor: small speed increase to the mcts visualization

--- visualize_mcts.py
+++ visualize_mcts.py
@@ -14,28 +14,23 @@ def write_mcts_as_png(search_paths, max_search_depth=float('inf'), path_to_file=
           'dir': '',
           'arrow': ''}
 
-  unique_nodes = []
-  for path in search_paths:
-    for node in path:
-      if node not in unique_nodes:
-        unique_nodes.append(node)
+  unique_nodes = set(n for p in search_paths for n in p)
 
-  seen = []
+  seen_nodes = []
   chosen_nodes = []
   for path in search_paths:
     search_depth = 0
     for i, node in enumerate(path):
+      if node not in seen_nodes:
+        if node.visit_count > 1:
+          if i == 0:
+            root_args = args.copy()
+            c, v = node.visit_count, round(node.value(), 2)
+            root_args['label'] = "c:{}, v:{}".format(c, v)
+            root_args['penwidth'] = 4
+            graph.add_node(pydot.Node(str(node), **root_args))
+            chosen_nodes.append(node)
 
-      if i == 0:
-        root_args = args.copy()
-        c, v = node.visit_count, round(node.value(), 2)
-        root_args['label'] = "c:{}, v:{}".format(c, v)
-        root_args['penwidth'] = 4
-        graph.add_node(pydot.Node(str(node), **root_args))
-        chosen_nodes.append(node)
-
-      if node not in seen:
-        if any([child in unique_nodes for child in node.children]):
           parent_id = str(node)
           chosen_action = np.argmax([child.visit_count for child in node.children])
           for action, child in enumerate(node.children):
@@ -49,7 +44,7 @@ def write_mcts_as_png(search_paths, max_search_depth=float('inf'), path_to_file=
             edge_args['fontsize'] = 10
 
             if child.visit_count > 0:
-              c, v = node.visit_count, round(node.value(), 2)
+              c, v = child.visit_count, round(child.value(), 2)
               node_args['label'] = "c:{}, v:{}".format(c, v)
               edge_args['label'] += ', r:{}'.format(round(child.reward, 2))
               node_args['arrow'] = 'forward'
@@ -71,7 +66,7 @@ def write_mcts_as_png(search_paths, max_search_depth=float('inf'), path_to_file=
             graph.add_node(pydot.Node(child_id, **node_args))
             graph.add_edge(pydot.Edge(parent_id, child_id, **edge_args))
 
-        seen.append(node)
+        seen_nodes.append(node)
 
       search_depth += 1
       if search_depth > max_search_depth:
